3902e874d74de5372355f3928587c29b967b3b05
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2022-07-02 20:43:51 -0400

    EPIC0001: tested button event/action for pr
    Took 1 hour 40 minutes

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,7 +3,7 @@
  */
 
 import { mount } from "@vue/test-utils";
-import MainNav from "@/components/main-nav/MainNav.vue";
+import MainNav from "../../../src/components/main-nav/MainNav.vue";
 
 describe("MainNav", () => {
   // company name test
@@ -39,17 +39,43 @@ describe("MainNav", () => {
    *       log in & out test
    *  ============================= */
   describe("when the user is logged out", () => {
-    // test log in
+    // test login
     it("should prompt the user to log in", () => {
-      const mainNavWrapper = mount(MainNav, {
-        data() {
-          return {
-            isLoggedIn: false,
-          };
-        },
-      });
+      const mainNavWrapper = mount(MainNav);
 
-      const loginButton = mainNavWrapper.findComponent("ActionButton");
+      /* instance of components to test */
+      const loginButton = mainNavWrapper.find("[data-test='login-button']");
+
+      // we expect the button to exist
+      expect(loginButton.exists()).toBe(true);
+    });
+
+    // should show user profile picture
+    describe("when user is logged out", () => {
+      // should display profile picture
+      it("should display user profile picture", async () => {
+        const mainNavWrapper = mount(MainNav);
+        
+        let profileImage = mainNavWrapper.find("[data-test='profile-image']");
+        // checking if the component exist
+        expect(profileImage.exists()).toBe(false);
+      
+        const loginButton = mainNavWrapper.find("[data-test='login-button']");
+        // trigger simulates an action/event & is async
+        await loginButton.trigger("click")
+        
+        // need to use the find method again because of the click event
+        profileImage = mainNavWrapper.find("[data-test='profile-image']");
+        // we expect the image to be there
+        expect(profileImage.exists()).toBe(true);
+      });
     });
   });
 });
+
+
+
+
+
+
+
